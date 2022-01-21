d64a07865e7d665af58911a239dca8916229d54f
Superdev0624 <superbaymax007@gmail.com>
2022-01-21 18:38:23 -0500

    update12

--- server.js
+++ server.js
@@ -26,10 +26,9 @@ app.set('view engine', 'html');
 app.use(bodyParser.json());
 app.use(cors());
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.use(function(req, res) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 /**
  * App Variables

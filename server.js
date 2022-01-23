89fb38d49147ee8b511043da8f93985cbca81e69
Superdev0624 <superbaymax007@gmail.com>
2022-01-23 09:53:15 -0500

    test

--- server.js
+++ server.js
@@ -69,9 +69,8 @@ app.get('/callback', function (req, res) {
     environment: 'sandbox',
     redirectUri: 'https://d1f3-188-43-136-33.ngrok.io/app',
   });
- console.log('aaaa',`https://d1f3-188-43-136-33.ngrok.io/app` + req.url)
   oauthClient
-    .createToken(`https://d1f3-188-43-136-33.ngrok.io/app` + req.url)
+    .createToken(`https://wepull-back.herokuapp.com` + req.url)
     .then(function (authResponse) {
       oauth2_token_json = JSON.stringify(authResponse.getJson(), null, 2);
     console.log(oauth2_token_json)
